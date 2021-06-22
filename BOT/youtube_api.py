08afd06992c51782774176727862715208633b8d
Purvesh Rathod <purvesh.r@turabit.com>
2021-06-22 13:47:28 +0530

    Logges added in youtube_api.py

--- BOT/youtube_api.py
+++ BOT/youtube_api.py
@@ -9,39 +9,45 @@ scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 class YoutubeAPI:
     def __init__(self):
-        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+        try:
+            os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+            api_service_name = "youtube"
+            api_version = "v3"
+            client_secrets_file = "BOT//client_secret.json"
 
-        api_service_name = "youtube"
-        api_version = "v3"
-        client_secrets_file = "BOT//client_secret.json"
-
-        # Get credentials and create an API client
-        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-            client_secrets_file, scopes)
-        credentials = flow.run_console()
-        self.youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, credentials=credentials)
+            # Get credentials and create an API client
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file, scopes)
+            credentials = flow.run_console()
+            self.youtube = googleapiclient.discovery.build(
+                api_service_name, api_version, credentials=credentials)
+            logging.info("An instance of YoutubeAPI is created.")
+        except Exception as exception:
+            logging.exception(exception)
 
     def get_playlist_data(self, playlist_id):
         try:
+            logging.info(f"Getting playlist data. PlaylistID: {playlist_id}")
             return self.youtube.playlistItems().list(
                 part='contentDetails, snippet',
                 playlistId=playlist_id
             ).execute()
-        except Exception as e:
-            logging.error(e)
+        except Exception as exception:
+            logging.exception(exception)
 
     def get_video_info(self, id):
         try:
+            logging.info(f"Getting video data. VideoID: {id}")
             return self.youtube.videos().list(
                 part="snippet",
                 id=id
             ).execute()
-        except Exception as e:
-            logging.error(e)
+        except Exception as exception:
+            logging.exception(exception)
 
     def insert_into_playlist(self, playlist_id, video_id):
         try:
+            logging.info(f"Inserting video in the playlist. \nVideoID: {video_id}\nPlaylistID: {playlist_id}")
             # response = get_video_info(youtube, video_id)
             video_payload = {
                 "snippet": {
@@ -58,5 +64,5 @@ class YoutubeAPI:
                 body=video_payload
             ).execute()
             logging.info("Resource added to the playlist")
-        except Exception as e:
-            logging.error(e)
+        except Exception as exception:
+            logging.exception(exception)
